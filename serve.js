ba6e04ebdac102a5b6dc85cdb1cdaf0947801cbd
Mert <mertbereket80@gmail.com>
2021-05-01 17:30:14 +0300

    bugfix

--- serve.js
+++ serve.js
@@ -7,12 +7,9 @@ app.get("/", function (req, res) {
   res.json("Headline Serve Project");
 });
 
-app.use(routers.lyricsRouter);
+
 app.use(routers.authRouter);
 app.use(routers.userRouter);
-app.use(routers.competitionRouter);
-app.use(routers.resultListRouter);
-app.use(routers.categoryRouter);
 
 app.use((req, res, next) => {
   res.status(HttpStatusCode.NOT_FOUND).send("404 NOT FOUND");
